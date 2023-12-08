021be4eb4cf5f7ee46fc0c1ce8a579885e42b285
m0ki <miquel.forcada@enti.cat>
2023-12-08 17:17:52 +0100

    Legs Finished

--- OctopusController/MyScorpionController.cs
+++ OctopusController/MyScorpionController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
+using static UnityEngine.GraphicsBuffer;
 
 
 namespace OctopusController
 {
-  
+
     public class MyScorpionController
     {
         //TAIL
@@ -19,17 +21,18 @@ namespace OctopusController
         //LEGS
         Transform[] legTargets;
         Transform[] legFutureBases;
-
         MyTentacleController[] _legs = new MyTentacleController[6];
 
-        Vector3[,] temPos;
+        bool startWalking = false;
+        double timer = 0;
+
 
         #region public
-        public void InitLegs(Transform[] LegRoots,Transform[] LegFutureBases, Transform[] LegTargets)
+        public void InitLegs(Transform[] LegRoots, Transform[] LegFutureBases, Transform[] LegTargets)
         {
             _legs = new MyTentacleController[LegRoots.Length];
             //Legs init
-            for(int i = 0; i < LegRoots.Length; i++)
+            for (int i = 0; i < LegRoots.Length; i++)
             {
                 _legs[i] = new MyTentacleController();
                 _legs[i].LoadTentacleJoints(LegRoots[i], TentacleMode.LEG);
@@ -39,7 +42,7 @@ namespace OctopusController
                 legFutureBases = LegFutureBases;
             }
 
-            temPos = new Vector3[_legs.Length, _legs[0].Bones.Length];
+
 
         }
 
@@ -59,13 +62,14 @@ namespace OctopusController
         //TODO: Notifies the start of the walking animation
         public void NotifyStartWalk()
         {
-
+            startWalking = true;
         }
 
         //TODO: create the apropiate animations and update the IK from the legs and tail
 
         public void UpdateIK()
         {
+            updateLegPos();
             updateLegs();
         }
         #endregion
@@ -75,9 +79,27 @@ namespace OctopusController
         //TODO: Implement the leg base animations and logic
         private void updateLegPos()
         {
-            //check for the distance to the futureBase, then if it's too far away start moving the leg towards the future base position
+            if (timer > 6 || !startWalking)
+            {
+                return;
+            }
 
+            timer += Time.deltaTime;
 
+            for (int i = 0; i < legFutureBases.Length; i++)
+            {
+                float moveAmount = 0;
+                if ((i % 2) == 0)
+                {
+                    moveAmount = Mathf.Sin(Time.time * 10) * Time.deltaTime;
+                }
+                else
+                {
+                    moveAmount = Mathf.Cos(Time.time * 10) * Time.deltaTime;
+                }
+
+                legFutureBases[i].Translate(Vector3.forward * moveAmount);
+            }
         }
         //TODO: implement Gradient Descent method to move tail if necessary
         private void updateTail()
@@ -87,89 +109,47 @@ namespace OctopusController
         //TODO: implement fabrik method to move legs 
         private void updateLegs()
         {
-            
-            for(int i = 0; i < _legs.Length; i++)
-            {
-                //if (_legs[i].LegDistances[2] < _legs[i].LegTotalDistance)
-                //{
-                    //Debug.Log("ENTERING BACKWARDS");
-                    
-                    backwardsIteration(i);
-                    //Debug.Log("ENTERING FORWARDS");
-
-                    forwardIteration(i);
-                //}
-               // else
-                //{
-                   // Debug.Log(i+ "_LEG NOT IN REACH");
-               // }
-                   
-
-            }
-
-
             for (int i = 0; i < _legs.Length; i++)
             {
-
-                _legs[i].Bones[0] = legFutureBases[i];
-
-                for (int j = 1; j < _legs[0].Bones.Length-1; j++)
-                {
-
-                    _legs[i].Bones[j].position = temPos[i, j];
-
-                    Debug.DrawRay(_legs[i].Bones[j].position, (_legs[i].Bones[j].position - _legs[i].Bones[j+1].position));
-
-                }
-                _legs[i].Bones[3] = legTargets[i];
+                backwardsIteration(i);
+                forwardIteration(i);
             }
-
-            
-
-
         }
 
         private void forwardIteration(int i)
         {
+            _legs[i].Bones[3].position = legTargets[i].position; // Assign Effector to Leg Targets
 
-            for (int j = 1; j < _legs[i].Bones.Length-1; j++)
+            for (int j = 1; j < _legs[i].Bones.Length - 1; j++)
             {
-                //Debug.Log(j);
+                Vector3 p = _legs[i].Bones[j].position - _legs[i].Bones[j + 1].position;
 
-                Vector3 p = _legs[i].Bones[j+1].position - _legs[i].Bones[j].position;
+                float v = _legs[i].LegDistances[j] / p.magnitude;
 
-                Vector3 v = _legs[i].LegDistances[j] * p.normalized;
+                Vector3 newPos = (1 - v) * _legs[i].Bones[j + 1].position + v * _legs[i].Bones[j].position;
 
-                Vector3 newPos = _legs[i].Bones[j].position + v ;
-
-                temPos[i, j] = newPos;
+                _legs[i].Bones[j].position = newPos;
             }
-
-
         }
 
         // Bones.length = 4, [3] -> EndEffector
 
         private void backwardsIteration(int i)
         {
+            _legs[i].Bones[0].position = legFutureBases[i].position; // Assign Joint0 to Future Base
 
-            for (int j = _legs[i].Bones.Length-1; j > 1; j--)
+            for (int j = _legs[i].Bones.Length - 1; j > 1; j--)
             {
-                //Debug.Log(j);
-
                 Vector3 p = _legs[i].Bones[j - 1].position - _legs[i].Bones[j].position;
 
-                //Debug.Log("j: " + j + " j-1: " + (j - 1));
+                float v = _legs[i].LegDistances[j - 1] / p.magnitude;
 
-                Vector3 v = _legs[i].LegDistances[j - 1] * p.normalized;
+                Vector3 newPos = (1 - v) * _legs[i].Bones[j - 1].position + v * _legs[i].Bones[j].position;
 
-                Vector3 newPos = _legs[i].Bones[j].position + v;
-
-                temPos[i, j] = newPos;
+                _legs[i].Bones[j].position = newPos;
             }
-
-
         }
+
         #endregion
     }
 }
